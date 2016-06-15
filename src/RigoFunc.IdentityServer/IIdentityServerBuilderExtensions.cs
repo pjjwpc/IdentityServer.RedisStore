ad63ddc7ee938a5da9117589245d8f8f498872c3
RigoFunc <rigofunc@outlook.com>
2016-06-15 18:20:02 +0800

    fix: fixed the Api CORS issues

--- src/RigoFunc.IdentityServer/IIdentityServerBuilderExtensions.cs
+++ src/RigoFunc.IdentityServer/IIdentityServerBuilderExtensions.cs
@@ -1,16 +1,20 @@
 ﻿// Copyright (c) RigoFunc (xuyingting). All rights reserved.
 
 using System;
+using System.Collections.Generic;
 using IdentityModel;
 using IdentityServer4;
 using IdentityServer4.Configuration;
+using IdentityServer4.Hosting.Cors;
 using IdentityServer4.Services;
 using IdentityServer4.Validation;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace RigoFunc.IdentityServer {
     public static class IIdentityServerBuilderExtensions {
@@ -21,6 +25,26 @@ namespace RigoFunc.IdentityServer {
             services.AddTransient<IProfileService, IdentityProfileService<TUser, TKey>>();
             services.AddTransient<IResourceOwnerPasswordValidator, IdentityResourceOwnerPasswordValidator<TUser>>();
             services.AddTransient<ICorsPolicyService, IdentityCorsPolicyService>();
+
+            var paths = new List<string>(Constants.RoutePaths.CorsPaths);
+            
+            // can this by reflection rather than hard code?
+            paths.Add("api/oauth/register");
+            paths.Add("api/oauth/sendcode");
+            paths.Add("api/oauth/login");
+            paths.Add("api/oauth/verifycode");
+            paths.Add("api/oauth/changepassword");
+            paths.Add("api/oauth/resetpassword");
+            paths.Add("api/oauth/update");
+            
+            // just for allow CORS for Api
+            services.AddTransient<ICorsPolicyProvider>(provider => {
+                return new PolicyProvider(
+                    provider.GetRequiredService<ILogger<PolicyProvider>>(),
+                    paths,
+                    provider.GetRequiredService<ICorsPolicyService>());
+            });
+            services.AddCors();
 
             services.Configure<IdentityOptions>(options => {
                 options.Cookies.ApplicationCookie.AuthenticationScheme = Constants.PrimaryAuthenticationType;
