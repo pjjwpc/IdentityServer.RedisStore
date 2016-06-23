4c63a4fe4dafa3c84f6fe845c48672c1eb10b523
RigoFunc <rigofunc@outlook.com>
2016-06-23 17:36:40 +0800

    refactor: remove the RigoFunc.Account project and dependency to RigoFunc.Account nuget package

--- src/RigoFunc.Account/Models/OpenIdLoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RigoFunc.Account.Models {
-    public class OpenIdLoginModel {
-        /// <summary>
-        /// 用户唯一标识.
-        /// </summary>
-        public string OpenId { get; set; }
-    }
-}
--- src/RigoFunc.Account/Models/SendCodeModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RigoFunc.Account.Models {
-    public class SendCodeModel {
-        /// <summary>
-        /// 电话号码.
-        /// </summary>
-        public string PhoneNumber { get; set; }
-    }
-}
