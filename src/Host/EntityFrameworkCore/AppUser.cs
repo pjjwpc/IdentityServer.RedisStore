7900cb880772062053f1b8ff2c315fb0abdcc3a1
RigoFunc <rigofunc@outlook.com>
2016-06-24 10:27:37 +0800

    refactor: tune the AppUser .ctor

--- src/Host/EntityFrameworkCore/AppUser.cs
+++ src/Host/EntityFrameworkCore/AppUser.cs
@@ -4,8 +4,8 @@ namespace Host.EntityFrameworkCore {
     public class AppUser : IdentityUser<int> {
         public AppUser() { }
 
-        public AppUser(string userName) : base(userName) {
-
+        public AppUser(string userName) : this() {
+            UserName = userName;
         }
     }
 }
