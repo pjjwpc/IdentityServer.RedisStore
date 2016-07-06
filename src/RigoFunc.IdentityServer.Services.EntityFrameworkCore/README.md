ab376a0df6d16b49285b9fd42d57fc187bc47364
liubinbin <lotosbin@gmail.com>
2016-07-06 17:59:25 +0800

    feature: implement IScopeStore,IClientStore with entityframeworkcore

---
+++ src/RigoFunc.IdentityServer.Services.EntityFrameworkCore/README.md
@@ -0,0 +1,7 @@
+﻿#
+
+# Usage
+```
+    var connectionString = @"Server=(local);Database=identityserver;Trusted_Connection=True;";
+    services.AddIdentityServerEntityFrameworkCoreServices(options => options.UseSqlServer(connectionString));
+```
---
+++ test/RigoFunc.IdentityServer.Services.EntityFrameworkCore.UnitTests/Services/EntityFrameworkCore/EntityFrameworkCoreScopeStoreTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RigoFunc.IdentityServer.Services.EntityFrameworkCore.UnitTests {
+    public class EntityFrameworkCoreScopeStoreTests : IClassFixture<IdentityDbContextFixture> {
+        IdentityDbContextFixture _fixture;
+
+        public EntityFrameworkCoreScopeStoreTests(IdentityDbContextFixture fixture) {
+            _fixture = fixture;
+        }
+        [Fact]
+        public void FindScopesAsyncTest() {
+            var scopeStore = new EntityFrameworkCoreScopeStore(_fixture.DbContext);
+            var scopes = scopeStore.FindScopesAsync(new string[] { "system" }).Result;
+            Assert.Single(scopes);
+            Assert.Equal("system", scopes.Single().Name);
+        }
+
+        [Fact]
+        public void GetScopesAsyncTest() {
+            var scopeStore = new EntityFrameworkCoreScopeStore(_fixture.DbContext);
+            {
+                var scopes = scopeStore.GetScopesAsync().Result;
+                Assert.NotEmpty(scopes);
+            }
+        }
+        [Fact]
+        public void GetScopesAsyncTest_publicOnly_false() {
+            var scopeStore = new EntityFrameworkCoreScopeStore(_fixture.DbContext);
+            {
+                var scopes = scopeStore.GetScopesAsync(publicOnly: false).Result;
+                Assert.NotEmpty(scopes);
+            }
+
+        }
+    }
+}
